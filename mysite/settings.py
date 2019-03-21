e97cf6445ab427af4c47d7369a905e0d89ed6f47
kyoheikokai <k.kyohei0915@gmail.com>
2019-03-21 17:41:37 +0900

    delete whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'school_system'
+    'school_system',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
